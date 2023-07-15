359b844cf5fbe585bda76cfe29b48de4c4496fc3
abhinav1912 <abhinavmathur1912@gmail.com>
2023-07-15 10:09:15 +0530

    first commit

---
+++ Notestar/Notestar/NotestarApp.swift
@@ -0,0 +1,15 @@
+//
+
+import SwiftUI
+
+@main
+struct NotestarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
