60441de8aabb97116885492760ce1dd227ff6b2f
abhinav1912 <abhinavmathur1912@gmail.com>
2023-07-16 19:21:15 +0530

    wip: note taking view

--- Notestar/Notestar/NoteView.swift
+++ Notestar/Notestar/NoteView.swift
@@ -6,12 +6,31 @@ import CoreData
 
 struct NoteView: View {
     private var note: Note
+    @State private var summary: String
+    @State private var content: String
+    @State private var title: String
 
     init(note: Note) {
         self.note = note
+        self.summary = note.summary ?? ""
+        self.content = note.body ?? ""
+        self.title = note.title ?? ""
     }
 
     var body: some View {
-        Text(note.title ?? "New Note!")
+        VStack {
+            TextField("New Note", text: self.$title)
+                .font(.title)
+                .fontWeight(.bold)
+            Divider()
+            TextField("Add a summary", text: self.$summary)
+                .padding(.top, 8)
+            Divider()
+            TextField("Add the note!", text: self.$content)
+                .padding(.top, 8)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding(.horizontal, 16)
+        .padding(.vertical, 16)
     }
 }
